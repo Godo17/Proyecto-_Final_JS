a1a53e5b299d0e83c55a15f69adef3284f874568
*Gabriel <*godo.nicolini@gmail.com*>
2024-06-17 17:34:11 -0300

    avance proyecto final

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
 
 const crearUsuario = (nombre,documento,tipologia,descripcion) => {
     const usuario = {
@@ -13,11 +13,38 @@ const crearUsuario = (nombre,documento,tipologia,descripcion) => {
 
     usuarios.push(usuario)
     localStorage.setItem("usuarios",JSON.stringify(usuarios))
+    return usuario
 }
 
-crearUsuario("Gabriel",36996737)
+crearUsuario()
 console.log(usuarios)
 
+const borrarTicket = (id) => {
+    usuarios = usuarios.filter(usuario => usuario.id != id)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+
+}
+
+borrarTicket()
+
+
+const visualizarTicket = (usuario) => {
+     const app = document.getElementById("app")
+     const element = document.createElement("div")
+     element.className = "ticket"
+     element.innerHTML = `
+                        <input type="text" class="input" value="${usuario.nombre}">
+                        <input type="text" class="input" value="${usuario.fecha}">
+                        <input type="text" class="input" value="${usuario.id}">
+                        <input type="text" class="input" value="${usuario.documento}">
+                        <input type="text" class="input" value="${usuario.tipologia}">
+                        <textarea type="text" class="detalle">${usuario.descripcion}</textarea>
+                        
+                        <button class="btnEjecutar btn-actualizar">Actualizar</button>
+                        <button class="btnEjecutar btn-eliminar">Eliminar</button>
+                        `
+    app.append(element)
+}
 const principal = () => {
     const btnEjecutar = document.getElementById("btnEjecutar")
         btnEjecutar.addEventListener("click",() => {
@@ -25,7 +52,8 @@ const principal = () => {
             const documentoUsuario = document.getElementById("documentoUsuario")
             const buscarTipologia = document.getElementById("buscarTipologia")
             const detalle = document.getElementById("detalle")
-            crearUsuario(nombreUsuario.value, documentoUsuario.value, buscarTipologia.value, detalle.value)
+            const usuario = crearUsuario(nombreUsuario.value, documentoUsuario.value, buscarTipologia.value, detalle.value)
+            visualizarTicket(usuario)
             //acá deberia crearse un Id random y mostrarse al usuario con un alert
             
             const nroTicket = crypto.randomUUID()
@@ -39,3 +67,4 @@ const principal = () => {
 }
 
 principal()
+
